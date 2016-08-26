da8c097df04c271d520efa28d1d83426ef110278
Alex <fix2008@ukr.net>
2016-08-26 18:13:04 +0300

    Adapter refactoring of Run

--- src/main/java/adapter/Run.java
+++ src/main/java/adapter/Run.java
@@ -3,18 +3,25 @@ package adapter;
 public class Run {
 
     public static void main(String[] args) {
-        adapter.simple.Adapter simple = new adapter.simple.AdapterImpl();
+        demoSimple();
+        demoInheritance();
+    }
 
-        System.out.println("Initial values in Adaptee:  12.3  17.6  11.9");
-        System.out.println(simple.convertFirstNumber());
-        System.out.println(simple.convertSecondNumber());
-        System.out.println(simple.convertThirdNumber());
-
+    private static void demoInheritance() {
         adapter.inheritance.Adapter inheritance = new adapter.inheritance.AdapterImpl();
 
         System.out.println("Initial values in Adaptee:  12.3  17.6  11.9");
         System.out.println(inheritance.convertFirstNumber());
         System.out.println(inheritance.convertSecondNumber());
         System.out.println(inheritance.convertThirdNumber());
+    }
+
+    private static void demoSimple() {
+        adapter.simple.Adapter simple = new adapter.simple.AdapterImpl();
+
+        System.out.println("Initial values in Adaptee:  12.3  17.6  11.9");
+        System.out.println(simple.convertFirstNumber());
+        System.out.println(simple.convertSecondNumber());
+        System.out.println(simple.convertThirdNumber());
     }
 }
