b06b899bfb3a642e4f1bda4b851eb2b508fe91d0
Oleksii Grytsiuk <crowfoot.ua@gmail.com>
2016-08-27 10:23:13 +0300

    Singleton reverted

--- src/main/java/singleton/Run.java
+++ src/main/java/singleton/Run.java
@@ -1,7 +1,5 @@
 package singleton;
 
-import singleton.simple.Singleton;
-
 public class Run {
 
     public static void main(String[] args) {
--- src/main/java/singleton/Singleton.java
+++ src/main/java/singleton/Singleton.java
@@ -1,4 +1,4 @@
-package singleton.simple;
+package singleton;
 
 public class Singleton {
     
