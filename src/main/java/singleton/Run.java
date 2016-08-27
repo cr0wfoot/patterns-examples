34fd3441d752e1cdc612c2529527805e22779a42
Oleksii Grytsiuk <crowfoot.ua@gmail.com>
2016-08-27 10:19:45 +0300

    Singleton refactoring

--- src/main/java/singleton/Run.java
+++ src/main/java/singleton/Run.java
@@ -1,5 +1,7 @@
 package singleton;
 
+import singleton.simple.Singleton;
+
 public class Run {
 
     public static void main(String[] args) {
--- src/main/java/singleton/simple/Singleton.java
+++ src/main/java/singleton/simple/Singleton.java
@@ -1,4 +1,4 @@
-package singleton;
+package singleton.simple;
 
 public class Singleton {
     
